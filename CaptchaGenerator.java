48fdc98a8d40b1615c95b6b08ffd6eb763b9db7c
thiyaroshni17 <thiyaroshni1717@gmail.com>
2024-07-11 22:14:06 +0530

    Create CaptchaGenerator.java

---
+++ CaptchaGenerator.java
@@ -0,0 +1,31 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.util.Random;
+
+public class CaptchaGenerator {
+    public String generateCaptchaCode(int length) {
+        String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        Random random = new Random();
+        StringBuilder captcha = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            captcha.append(chars.charAt(random.nextInt(chars.length())));
+        }
+        return captcha.toString();
+    }
+
+    public BufferedImage createCaptchaImage(String captcha) {
+        int width = 160;
+        int height = 60;
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        Graphics g = image.getGraphics();
+        g.setColor(Color.WHITE);
+        g.fillRect(0, 0, width, height);
+        g.setFont(new Font("Arial", Font.BOLD, 40));
+        g.setColor(Color.BLACK);
+        g.drawString(captcha, 20, 45);
+        g.dispose();
+        return image;
+    }
+}
